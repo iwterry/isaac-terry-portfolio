88bfadb057f7217ba6cc68514818e5ac9bf4a62c
Isaac Terry <iwterry@iupui.edu>
2015-08-07 17:33:55 -0400

    Change media query for smaller window sizes or devices, and modified jquery code to have effects be quicker

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,11 @@
 $(document).ready( function() {
 	$('section h2').siblings().hide(); // only want the questions to be seen for the sections 
-	$('header').show('2000', function() { // show background and the text in the header 
+	$('header').show('1000', function() { // show background and the text in the header 
 		// give user instructions for how to proceed 
 		alert('Please click on the questions below to expand or collapse the responses to them.');	
 	}); 
 	//allow the content under the main question of each section to expand or collapse by clicking the question  
 	$('section h2').click( function() {
-		$(this).siblings().toggle(1000);
+		$(this).siblings().toggle(400);
 	});
 });
