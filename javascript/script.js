a036802a945181158fc7d856531ce5b4aea39cf5
Isaac Terry <iwterry@iupui.edu>
2015-08-08 21:23:14 -0400

    Increase font-size for h3 elements on smaller devices and remove header from not being displayed

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,7 @@
 $(document).ready( function() {
 	$('section h2').siblings().hide(); // only want the questions to be seen for the sections 
-	$('header').show('1000', function() { // show background and the text in the header 
 		// give user instructions for how to proceed 
-		alert('Please click on the questions below to expand or collapse the responses to them.');	
-	}); 
+	alert('Please click on the questions below to expand or collapse the responses to them.');	
 	//allow the content under the main question of each section to expand or collapse by clicking the question  
 	$('section h2').click( function() {
 		$(this).siblings().toggle(400);
